63c00b43896bfa78bb023cbd3379470ccb290a77
Adam <adambeck62@yahoo.com>
2020-11-25 12:47:08 +0000

    XML for adding and view project completed. Ready for connection to Room Database

--- app/src/main/java/com/example/buildmate/MainActivity.java
+++ app/src/main/java/com/example/buildmate/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class HomeScreen extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
     private Button projectNavigation;
     private Button styleNavigation;
     private Button materialsNavigation;
--- app/src/main/java/com/example/buildmate/Project.java
+++ app/src/main/java/com/example/buildmate/Project.java
@@ -2,13 +2,42 @@ package com.example.buildmate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Project extends AppCompatActivity {
-
+    private Button viewProject;
+    private Button newProject;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project);
+        viewProject = (Button) findViewById(R.id.viewProjectButton);
+        viewProject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewProjectScreen();
+            }
+        });
+
+        newProject = (Button) findViewById(R.id.newProjectButton);
+        newProject.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newProjectScreen();
+            }
+        });
+
+
+    }
+    public void viewProjectScreen(){
+        Intent openViewProjectScreen = new Intent(this, ViewProject.class);
+        startActivity(openViewProjectScreen);
+    }
+    public void newProjectScreen(){
+        Intent openNewProjectScreen = new Intent(this, AddProject.class);
+        startActivity(openNewProjectScreen);
     }
 }
