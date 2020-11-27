67ab41229d5521c88622fb7917bb3159a99ca001
Adam <adambeck62@yahoo.com>
2020-11-27 11:04:20 +0000

    Adding comments to make the prototype code more readable

--- app/src/main/java/com/example/buildmate/ViewProjectAdapter.java
+++ app/src/main/java/com/example/buildmate/ViewProjectAdapter.java
@@ -11,12 +11,12 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
-public class ProjectAdapter extends RecyclerView.Adapter<ProjectAdapter.ProjectViewHolder> {
+public class ViewProjectAdapter extends RecyclerView.Adapter<ViewProjectAdapter.ProjectViewHolder> {
 
     private Context context;
     private List<ProjectEntity> projectEntityList;
 
-    public ProjectAdapter(Context context){
+    public ViewProjectAdapter(Context context){
         this.context = context;
 
     }
@@ -28,13 +28,14 @@ public class ProjectAdapter extends RecyclerView.Adapter<ProjectAdapter.ProjectV
 
     @NonNull
     @Override
-    public ProjectAdapter.ProjectViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    //Creating the view holder with an inflater to display the results of the project_row.xml file
+    public ViewProjectAdapter.ProjectViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
        View projectView = LayoutInflater.from(context).inflate(R.layout.project_row,parent,false);
        return new ProjectViewHolder(projectView);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ProjectAdapter.ProjectViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewProjectAdapter.ProjectViewHolder holder, int position) {
         holder.projectNameRow.setText(this.projectEntityList.get(position).projectName);
         holder.streetLocationRow.setText(this.projectEntityList.get(position).projectStreetLocation);
         holder.cityRow.setText(this.projectEntityList.get(position).projectCity);
@@ -44,8 +45,10 @@ public class ProjectAdapter extends RecyclerView.Adapter<ProjectAdapter.ProjectV
 
     @Override
     public int getItemCount() {
+
         return projectEntityList.size();
     }
+
 
     public class ProjectViewHolder extends RecyclerView.ViewHolder{
         TextView projectNameRow;
