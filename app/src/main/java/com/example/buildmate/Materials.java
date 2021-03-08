711e956ca2aa4814ca7add1c33fc4bfd14694c79
adambeck123 <74963294+adambeck123@users.noreply.github.com>
2021-03-08 11:11:02 +0000

    AddMaterials java class added + navigation from the Materials page

--- app/src/main/java/com/example/buildmate/Materials.java
+++ app/src/main/java/com/example/buildmate/Materials.java
@@ -2,13 +2,23 @@ package com.example.buildmate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Materials extends AppCompatActivity {
+    private Button materialsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_materials);
+        materialsButton = (Button) findViewById(R.id.addMaterials);
+        materialsButton.setOnClickListener(v -> openAddMaterials());
+    }
+
+    public void openAddMaterials(){
+        Intent navigateToStyleScreen = new Intent(this, AddMaterials.class);
+        startActivity(navigateToStyleScreen);
     }
 }
