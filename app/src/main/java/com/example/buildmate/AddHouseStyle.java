0bb7a127ecb521cc2b92e68ad41d360d8705a703
adambeck123 <74963294+adambeck123@users.noreply.github.com>
2021-03-08 11:03:06 +0000

    AddHouseStyle class added + navigation from the housestyle page

---
+++ app/src/main/java/com/example/buildmate/AddHouseStyle.java
@@ -0,0 +1,44 @@
+package com.example.buildmate;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddHouseStyle extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_housestyle);
+/*
+        final EditText newProjectName = findViewById(R.id.newProjectName);
+        final EditText newStreetLocation = findViewById(R.id.newStreetLocation);
+        final EditText newCity = findViewById(R.id.newCity);
+        final EditText newPostcode = findViewById(R.id.newPostcode);
+        final String username = "User1";
+        Button projectSubmitButton = findViewById(R.id.projectSubmitButton);
+        projectSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addProjectToDatabase(newProjectName.getText().toString(), newStreetLocation.getText().toString(),newCity.getText().toString(),newPostcode.getText().toString(),username );
+            }
+        });*/
+    }
+/*
+    private void addProjectToDatabase(String projectName, String streetLocation, String city, String postcode, String username) {
+        BuildMateDatabase projectDatabase = BuildMateDatabase.getBuildMateInstance(this.getApplicationContext());
+        ProjectEntity projectEntity = new ProjectEntity();
+        projectEntity.projectName = projectName;
+        projectEntity.projectStreetLocation = streetLocation;
+        projectEntity.projectCity =city;
+        projectEntity.projectPostcode = postcode;
+        projectDatabase.projectDao().createProject(projectEntity);
+
+        finish();
+
+    }
+*/
+}
--- app/src/main/java/com/example/buildmate/Style.java
+++ app/src/main/java/com/example/buildmate/Style.java
@@ -2,13 +2,25 @@ package com.example.buildmate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Style extends AppCompatActivity {
 
+    public Button HouseStyle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_style);
+
+        HouseStyle = (Button) findViewById(R.id.newHouseStyle);
+        HouseStyle.setOnClickListener(v -> openAddHouseStyle());
+
+    }
+    public void openAddHouseStyle(){
+        Intent navigateToStyleScreen = new Intent(this, AddHouseStyle.class);
+        startActivity(navigateToStyleScreen);
     }
 }
